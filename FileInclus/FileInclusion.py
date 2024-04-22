1bc382570a66341cf3556a325d4e7b6576c3e333
Martin Vidlicka <119677428+rbmartin1@users.noreply.github.com>
2024-04-22 20:45:49 +0200

    Update FileInclusion.py

--- FileInclus/FileInclusion.py
+++ FileInclus/FileInclusion.py
@@ -5,15 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Base directory from where the Flask app starts looking for files
+    # zakladny priecinok
     base_dir = 'FileInclus'
 
-    # Get the 'file' parameter from the URL, if not found use 'data/default.txt' as the default
+    # ziskanie file_name z url ak nie tak 'data/default.txt'
     file_name = request.args.get('file', 'data/default.txt')
 
-    # Create the full file path
+    # vytvorenie cesty
     file_path = os.path.join(base_dir, file_name)
 
+    #jednoduchy white list pre jedine 2 dovolene subory
     if 'default.txt' not in file_path and 'hidden.txt' not in file_path:
         return "Access denied."
 
@@ -23,7 +24,7 @@ def index():
             if content == "":
                 print(f"The file {file_path} is empty.")
             else:
-                print(f"Content of {file_path}: '{content}'")  # Print content to the console for debugging
+                print(f"Content of {file_path}: '{content}'")
     except FileNotFoundError:
         content = f"File not found. Tried accessing: {file_path}"
         print(content)
@@ -31,9 +32,25 @@ def index():
         content = f"An error occurred: {str(e)}"
         print(content)
 
-    # Explicitly create a response object with the correct content type
+    # Explicitné vytvorenie odpovede so správnym typom
     response = Response(content, mimetype='text/plain; charset=utf-8')
     return response
+
+
+
+if __name__ == '__main__':
+    print(f"Current working directory: {os.getcwd()}")
+    app.run(debug=True)
+
+
+#http://127.0.0.1:5000/?file=default.txt
+#Content of FileInclus\data/default.txt: 'This is a safe file.'
+#http://127.0.0.1:5000/?file=hidden.txt
+#Content of FileInclus\data/hidden.txt: 'You have accessed the hidden file.'
+
+#http://127.0.0.1:5000/?file=../secret.txt
+#Acess denied 
+
 
 
 
